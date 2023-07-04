b5d1e769e0b2dfa59dafdc461b095db17392b639
girik4 <giriksoni4@gmail.com>
2023-07-03 22:38:26 -0500

    weather app works

---
+++ src/api.js
@@ -0,0 +1,11 @@
+export const url = 'https://wft-geo-db.p.rapidapi.com/v1/geo/cities';
+export const options = {
+    method: 'GET',
+    headers: {
+        'X-RapidAPI-Key': 'b662090c98msh9c0cd73276affedp1c4248jsn3c4828aa4e34',
+        'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+    }
+};
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "f8c5c225e94f3d3d94316244957290fd";
